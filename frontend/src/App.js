ae390a17a767431e0810ac5ca10f23354d59faa6
AnkitPakhale <akp3067@gmail.com>
2023-02-01 23:05:44 +0530

    Added: frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
