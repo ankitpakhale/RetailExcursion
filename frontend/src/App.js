054b7e44defd6273aaa8276601966663d28eadc8
AnkitPakhale <akp3067@gmail.com>
2023-02-02 22:06:37 +0530

    Added: product, rating and homescreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
