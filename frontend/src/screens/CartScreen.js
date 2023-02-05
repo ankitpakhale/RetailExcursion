cb3b58e3ab375802b0b26b7e295e40cb5a949c82
AnkitPakhale <akp3067@gmail.com>
2023-02-05 17:04:09 +0530

    Added: cart data into localstorage using redux

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  Card,
+  Form,
+  Button,
+  Col,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams, useLocation } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
+import Message from "../components/Message";
 
 const CartScreen = () => {
-  return (
-    <div>CartScreen</div>
-  )
-}
+  const productId = useParams().id;
+  const location = useLocation(); // to get url data
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1; // spliting url data to get qty
 
-export default CartScreen
+  const dispatch = useDispatch();
+
+  // to pull the data we use useSelector
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.info("cartItems: ", cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return <div>CartScreen</div>;
+};
+
+export default CartScreen;
