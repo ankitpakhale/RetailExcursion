ca9ba6758b27c2227d1a8e35db5ee743ca9376ac
AnkitPakhale <akp3067@gmail.com>
2023-02-11 22:34:03 +0530

    Added: register api and auth permissions

--- base/views.py
+++ base/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+# project modules import
 from .products import products
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+# rest_framework imports
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status
 
 # Customizing JWT token
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -21,33 +29,66 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         
         # getting data from serializer itself
         serializer = UserSerializerWithToken(self.user).data
-        for k, v in serializer.items():
-            custom_data[k] = v
+        for key, value in serializer.items():
+            custom_data[key] = value
 
         return custom_data
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-    
-    
+
+# @api_view(['POST'])
+# def registerUser(request):
+#     data = request.data
+#     try:
+#         User.objects.get(
+#             first_name = data['name'],
+#             username = data['username'],
+#             email = data['email'],
+#         )
+#         message = {'detail': 'User with this email already exist'}
+#         return Response(message, status = status.HTTP_400_BAD_REQUEST)
+#     except Exception:
+#         user = User.objects.create(
+#             first_name = data['name'],
+#             username = data['username'],
+#             email = data['email'],
+#             password = make_password(data['password'])
+#         )
+#         serializer = UserSerializerWithToken(user, many=False)
+#         return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        # It will automatically check duplication, it duplicate data found then it will throw error
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['username'],
+            email = data['email'],
+            password = make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    except Exception:
+        message = {'detail': 'User with this email already exist'}
+        return Response(message, status = status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        
-        '/api/products/upload/',
-        
-        '/api/products/<id>/reviews/',
-    
-        '/api/products/top/',
-        '/api/products/<id>/',
-    
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
-    
-    ]
-    return Response(routes)
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many = True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
@@ -60,10 +101,3 @@ def getProduct(request, pk):
     productData = Product.objects.get(_id = pk)
     serializer = ProductSerializer(productData)
     return Response(serializer.data)
-
-@api_view(['GET'])
-def getUserProfile(request):
-    user = request.user
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
-
