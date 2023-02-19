3b5051cb212741cddf549b99ceabd39208d629b6
AnkitPakhale <akp3067@gmail.com>
2023-02-19 14:14:25 +0530

    Created: addOrderItems API

--- base/serializers.py
+++ base/serializers.py
@@ -1,26 +1,28 @@
+from ctypes import addressof
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import *
-
+from .models import Product, Order, OrderItem, ShippingAddress
 
 
 class UserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField(read_only = True)
-    _id = serializers.SerializerMethodField(read_only = True)
-    isAdmin = serializers.SerializerMethodField(read_only = True)
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['_id', 'username', 'email', 'name', 'isAdmin'] 
+        fields = ['_id', 'username', 'email', 'name', 'isAdmin']
         # fields = '__all__'
 
     # function name should always start with get_ and end with with field name
+    # field name is created just below the class started
     def get_isAdmin(self, obj):
         return obj.is_staff
-        
+
     def get__id(self, obj):
         return obj.id
-         
+
     def get_name(self, obj):
         name = obj.first_name
         if name == "":
@@ -29,20 +31,61 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserSerializerWithToken(UserSerializer):
-    token = serializers.SerializerMethodField(read_only = True)
+    token = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ['_id', 'username', 'email', 'name', 'isAdmin', 'token'] 
+        fields = ['_id', 'username', 'email', 'name', 'isAdmin', 'token']
 
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        # fields = ['name', 'image', 'brand', 'category', 'description', 'rating', 'numReviews', 'price', 'countInStock', 'createdAt', '_id'] 
-        fields = '__all__' 
+        # fields = ['name', 'image', 'brand', 'category', 'description', 'rating', 'numReviews', 'price', 'countInStock', 'createdAt', '_id']
+        fields = '__all__'
 
 
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
 
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    # function name should always start with get_ and end with with field name
+    # field name is created just below the class started
+    def get_orders(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(
+                obj.shippingAddress, many=False)
+        except Exception:
+            address = False
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user, many=False)
+        return serializer.data
